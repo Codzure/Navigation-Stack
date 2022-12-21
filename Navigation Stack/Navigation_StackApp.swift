65481bdf483707e2d7b51b80c7babaa80f4b10a1
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-22 01:03:31 +0300

    Initial Commit

---
+++ Navigation Stack/Navigation_StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_StackApp.swift
+//  Navigation Stack
+//
+//  Created by Leonard Mutugi on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
